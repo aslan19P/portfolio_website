91081c50a9290acbfa69b2ea27facc1fe999f5cc
aslan19P <matackubovaslan@gmail.com>
2025-06-30 17:46:48 +0500

    fix

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,3 @@
-import { links } from "./data";
+import { getLinks } from "./data";
 
-export type SectionName = (typeof links)[number]["name"];
+export type SectionName = (ReturnType<typeof getLinks>)[number]["name"];
